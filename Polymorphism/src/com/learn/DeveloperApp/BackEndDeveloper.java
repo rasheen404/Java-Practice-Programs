6832439644767d74d8ae31be5e5b2d6fcc4b94f0
Rasheen <rasheenerror404@gmail.com>
2023-11-30 11:09:26 +0530

    Created a Polymorphism program of DeveloperApp

---
+++ Polymorphism/src/com/learn/DeveloperApp/BackEndDeveloper.java
@@ -0,0 +1,16 @@
+package com.learn.DeveloperApp;
+
+public class BackEndDeveloper extends Developer{
+	void AttendMeeting() {
+		System.out.println("Developer is Attending Meeting.");
+	}
+
+	void Project() {
+		System.out.println("Developer is doing Project.");
+	}
+
+	void LearnJava() {
+		System.out.println("BackEnd-Developer is Learning Java.");
+	}
+
+}
