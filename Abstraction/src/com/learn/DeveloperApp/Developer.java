e7a81b1e17c9161e9afaffb974cb5223a5891daa
Rasheen <rasheenerror404@gmail.com>
2023-12-02 17:47:35 +0530

    Created a Abstraction Program of DeveloperApp

---
+++ Abstraction/src/com/learn/DeveloperApp/Developer.java
@@ -0,0 +1,12 @@
+package com.learn.DeveloperApp;
+
+abstract class Developer {
+	
+	void attendMeeting()
+	{
+		System.out.println("Developer is Attending Meeting.");
+	}
+	
+	abstract void doProject();
+
+}
---
+++ Abstraction/src/com/learn/DeveloperApp/PythonDeveloper.java
@@ -0,0 +1,16 @@
+package com.learn.DeveloperApp;
+
+public class PythonDeveloper extends Developer{
+	
+	@Override
+	void doProject()
+	{
+		System.out.println("Python Developer is doing python project.");
+	}
+	
+	void learnPython()
+	{
+		System.out.println("Python Developer is learning Python.");
+	}
+
+}
