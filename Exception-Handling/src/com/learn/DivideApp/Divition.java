b2cee01a6ca71d04db6167b2fd1bbb9784c71444
Rasheen <rasheenerror404@gmail.com>
2023-12-04 12:35:25 +0530

    Created a program of Try Catch in Exception Handler name is DivitionApp

---
+++ Exception-Handling/src/com/learn/DivideApp/Divition.java
@@ -0,0 +1,28 @@
+package com.learn.DivideApp;
+
+import java.util.Scanner;
+
+public class Divition {
+	public static void main(String[] args) {
+		try {
+			System.out.println("Program started Execution.");
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.print("Enter the First Number : ");
+			int a = scanner.nextInt();
+
+			System.out.print("Engter the second Number : ");
+			int b = scanner.nextInt();
+
+			int c = a / b;
+			System.out.println("The result is : " + c);
+
+		} catch (Exception e) {
+			System.out.println("Exception is Handled.");
+
+		} finally {
+			System.out.println("Program execution Ended.");
+		}
+	}
+
+}
