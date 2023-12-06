2aeb61815f514a7a4c9498e69a3962a5c0619648
Rasheen <rasheenerror404@gmail.com>
2023-12-06 19:33:57 +0530

    Created a Thread.sleep program

---
+++ Multi-Threading/src/com/sleep/ex1/program1.java
@@ -0,0 +1,44 @@
+package com.sleep.ex1;
+
+public class program1 {
+
+	public static void main(String[] args) {
+		PrintNum();
+		PrintChar();
+	}
+		
+	
+	public static void PrintNum()
+	{
+		System.out.println("Number started Printing");
+		for(int i =1 ;i<=5;i++)
+		{
+			System.out.println(i);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Number Ended Printing");
+	}
+	
+	public static void PrintChar()
+	{
+		System.out.println("Character started Printing");
+		for(char i ='A' ;i<='E';i++)
+		{
+			System.out.println(i);
+
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			
+		}
+	}
+		System.out.println("Number Ended Printing");
+	}
+}
