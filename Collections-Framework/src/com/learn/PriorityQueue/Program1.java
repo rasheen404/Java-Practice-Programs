3f385d16f25a99063a97234c5fde6a1ae7caa482
Rasheen <rasheenerror404@gmail.com>
2023-12-11 13:20:15 +0530

    Created first program of PriorityQueue in Collection frameworks

---
+++ Collections-Framework/src/com/learn/PriorityQueue/Program1.java
@@ -0,0 +1,19 @@
+package com.learn.PriorityQueue;
+
+import java.util.PriorityQueue;
+
+public class Program1 {
+public static void main(String[] args) {
+	PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>();
+	
+	
+	pQueue.add(50);
+	pQueue.add(30);
+	pQueue.add(40);
+	pQueue.add(10);
+	pQueue.add(20);
+	
+	
+	System.out.println(pQueue);
+}
+}
