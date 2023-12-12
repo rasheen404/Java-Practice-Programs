334b5474f6357b3f532791f472134bad362e3d0b
Rasheen <rasheenerror404@gmail.com>
2023-12-12 21:21:36 +0530

    Created first program1 of TreeSet in Collections-Framework

---
+++ Collections-Framework/src/com/learn/TreeSet/Program1.java
@@ -0,0 +1,20 @@
+package com.learn.TreeSet;
+
+import java.util.TreeSet;
+
+public class Program1 {
+	public static void main(String[] args) {
+
+		TreeSet treeSet = new TreeSet();
+
+		treeSet.add(50);
+		treeSet.add(30);
+		treeSet.add(40);
+		treeSet.add(10);
+		treeSet.add(20);
+
+		System.out.println(treeSet);
+
+	}
+
+}
