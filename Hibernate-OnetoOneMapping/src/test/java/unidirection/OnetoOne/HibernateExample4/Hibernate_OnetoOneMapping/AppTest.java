1733cf57b52ca894d68f51b72b202b1a7a68a675
Rasheen <rasheenerror404@gmail.com>
2023-12-28 17:57:40 +0530

    Created a Hibernate program for unidirectional ONE to ONE mapping

---
+++ Hibernate-OnetoOneMapping/src/test/java/unidirection/OnetoOne/HibernateExample4/Hibernate_OnetoOneMapping/AppTest.java
@@ -0,0 +1,38 @@
+package unidirection.OnetoOne.HibernateExample4.Hibernate_OnetoOneMapping;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
